4b53d2bc53c8863c258017a769888dd1bdf61e21
nerdbuilt <77251080+nerdbuilt@users.noreply.github.com>
2026-03-27 15:22:32 -0400

    Update trigger-connector-based-on-cav-value.js

--- trigger-connector-based-on-cav-value.js
+++ trigger-connector-based-on-cav-value.js
@@ -1,227 +1,231 @@
+define('3rdparty.bundle', [], function () {
+    console.log("#### 3rdparty.bundle.js loaded");
 
-    // This customization sets the Salesforce.salesforce_id CAV on the call
-    // when the agent selects a new related-to record in the adapter and
-    // also asks the agent to verify the disposition action if the record
-    // selected does not match the record in focus.
+    // let _five9Metadata = null;
+    // let _activeCall = null;
 
-    // IMPORTANT - CAV MUST be on the campaign profile layout or this
-    // they cannot be updated in the customization.
+    
+    // let freedomMetadataURL = "https://app.five9.com/appsvcs/rs/svc/auth/metadata";
+    // let contextPaths = {
+    //     "agent_rest": "/appsvcs/rs/svc",
+    //     "agent_str": "/strsvcs/rs/svc",
+    //     "sup_rest": "/supsvcs/rs/svc"
+    // };
+    // let base_api_url;
+    // let base_agents_api_url;
+    // let f9OrgId;
+    // let f9UserId;
 
-
-    // const hookApi = window.Five9.CrmSdk.hookApi();
-    const interactionApi = window.Five9.CrmSdk.interactionApi();
-
-    // Custom logging class to add a prefix and timestamp to the console log for easy identification
-    // initialize logging variables  
-    const defaultLoggingPrefix = "[*****]";
-    const allowedLoggingModes = ["error", "warn", "log", "info", "debug"];
-    class consoleLogger {
-      constructor(prefix = defaultLoggingPrefix) {
-        this.prefix = prefix;
-
-        // for each mode in allowedLoggingModes, create a function
-        // that will log the message with the mode
-        allowedLoggingModes.forEach((mode) => {
-          this[mode] = (...messages) => {
-            const d = new Date();
-            console[mode](
-              `${this.prefix} (${d.toLocaleString(undefined, {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                hour12: false, // use 24-hour time format
-              })}) ${messages
-                .map((message) => {
-                  if (typeof message === "object") {
-                    return `\n${JSON.stringify(message)}`;
-                  } else {
-                    return message;
-                  }
-                })
-                .join(" ")}`
-            );
-          };
-        });
-      }
-    }
-    const console2 = new consoleLogger();
-
-    console2.info("Customization loaded.")
-
-    class DomainCAVs {
-      constructor(cavList) {
-        // this.cavList = cavList;
-        cavList.forEach((cav) => {
-          this[cav.group] = this[cav.group] || {};
-          this[cav.group][cav.name] = cav;
-        });
-        console2.debug("CAV Definitions:", this);
-      }
-    }
-
-    // initialize f9libLoaded scope variables
-    // dictionary to hold the domain CAVs for easy lookup
-    let cavDefinitions = undefined;
-
-    // tracking variable for the selected record id
-    // let selectedRecordId = "";
-    let currentInteractionId = "";
-
-    // update the Salesforce.salesforce_id CAV on the call
-    // function updateSalesforceIdCAV(salesforceId) {
-    //   let updateCavList = [
-    //     {
-    //       id: cavDefinitions.Salesforce.salesforce_id.id,
-    //       value: salesforceId,
-    //     },
-    //   ];
-    //   console2.log("CAV update with cavList:", updateCavList);
-    //   interactionApi.setCav(
-    //     {
-    //       interactionId: currentInteractionId,
-    //       cavList: updateCavList
-    //     });
-    //   console2.log(`CAV update complete`);
+    // async function initialize() {
+    //     try {
+    //         const response = await fetch(freedomMetadataURL, { credentials: 'include' });
+    //         const data = await response.json();
+    //         f9OrgId = data.orgId;
+    //         f9UserId = data.userId;
+    //         base_api_url = "https://" + data.metadata.dataCenters[0].apiUrls[0].host + ":" + data.metadata.dataCenters[0].apiUrls[0].port;
+    //         base_agents_api_url = base_api_url + contextPaths.agent_rest;
+    //         base_supervisor_api_url = base_api_url + contextPaths.sup_rest;
+    //         console.log('####', "user ID: " + data.userId);
+    //         console.log('####', "Base Supervisor API URL: " + base_supervisor_api_url);
+    //         console.log('####', "Base Agent API URL: " + base_agents_api_url);
+    //         console.log('####', JSON.stringify(data));
+    //         return data;
+    //     } catch (error) {
+    //         console.log('####', "You must be logged into Five9 in another browser tab for this to work");
+    //         throw error;
+    //     }
     // }
 
-    // register callbacks to the interaction API
-    interactionApi.subscribe({
 
-      // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
-      callStarted: (startedCall) => {
-        console2.log("Call Started", startedCall);
-        currentInteractionId = startedCall.callData.interactionId;
-        interactionApi
-          .getCav({ interactionId: startedCall.callData.interactionId })
-          .then((domainCavsReturned) => {
-            cavDefinitions = new DomainCAVs(domainCavsReturned);
-            console2.log("Interaction API got cavList:", cavDefinitions);
-          });
-      },
+    // async function getDomainCallVariables() {
 
-      // on call accepted, get the domain CAVs and map to a dictionary for easy lookup
-      callAccepted: (acceptedCall) => {
-        console2.log(`Call Accepted ${JSON.stringify(acceptedCall)}`);
-
-      },
-
-      // on object selected, update the CAV
-      // objectSelected: (selectedObject) => {
-      //   console2.log("Object Selected:", selectedObject);
-
-      //   let selectedObjectRecordId = selectedObject.crmObject.id || undefined;
-      //   console2.log("Selected Record Id:", selectedObjectRecordId);
-      //   if (selectedObjectRecordId != undefined) {
-      //     let urlParts = selectedObject.crmObject.metadata.url.split("/");
-      //     // console2.debug("URL Parts:", urlParts);
-      //     // for each part of the url, check if it is the record id
-      //     urlParts.forEach((part) => {
-      //       if (part.includes(selectedObjectRecordId)) {
-      //         selectedRecordId = part;
-      //         console2.log(`Found in URL: ${selectedRecordId}`);
-      //       }
-      //     })
-      //   } else {
-      //     selectedRecordId = "";
-      //   }
-
-      //   console2.log(`Selected Record Id IS NOW: ${selectedObjectRecordId}`);
-      //   // IMPORTANT - The CAV MUST be on the campaign profile layout
-      //   updateSalesforceIdCAV(selectedRecordId);;
-      // },
-
-      // call accepted comes after call start event, leaving here in case a future action is needed
-      callAccepted: (acceptedCall) => {
-        console2.log("Call Accepted:", acceptedCall);
-      },
-    });
-
-    // register callbacks to the hook API
-    // hookApi.registerApi({
-    //   // on screen pop, set the selectedRecordId
-    //   afterScreenPop: function (screenPopData) {
-    //     console2.log("afterScreenPop:", screenPopData);
-    //     // if only one screen pop object is returned, set the selected record id and update the CAV
-    //     if (screenPopData.screenPopObjects.length === 1) {
-    //       selectedRecordId = screenPopData.screenPopObjects[0].params.recordId;
-    //       updateSalesforceIdCAV(selectedRecordId);
-    //     }
-    //     return Promise.resolve({
-    //       status: {
-    //         statusCode: Five9.CrmSdk.HookStatusCode.Proceed,
-    //       },
+    //     console.log('####', "getDomainCallVariables()");
+    //     const endpointURL = `${base_agents_api_url}/orgs/${f9OrgId}/call_variables`;
+    //     console.log('####', endpointURL);
+    //     const response = await fetch(endpointURL, { credentials: 'include' });
+    //     const res = await response.json();
+    //     console.log('####', "Domain CAVs:");
+    //     console.log('####', res);
+    //     const cavs = {};
+    //     res.forEach((cav) => {
+    //         cavs[cav.group] = cavs[cav.group] || {};
+    //         cavs[cav.group][cav.name] = cavs[cav.group][cav.name] || {};
+    //         cavs[cav.group][cav.name]["id"] = cav.id;
+    //         cavs[cav.group][cav.name]["type"] = cav.type;
+    //         cavs[cav.group][cav.name]["restrictions"] = cav.restrictions;
     //     });
-    //   },
+    //     console.log('####', "Domain CAVs after transformation:");
+    //     console.log('####', cavs);
+    //     return cavs;
+    // }
 
-    //   // on disposition, check if the CAV is set to the correct record id
-    //   // NOTE - it is possible to still have the CAV set to the wrong record id
-    //   //        if the agent disposition timer expires and the call is dispositioned
-    //   beforeDisposition: function (dispositionData) {
-    //     console2.log("beforeDisposition:", dispositionData);
-    //     // get the in-focus record id from the component
-    //     let inFocusRecordId = cmp.get("v.recordId");
-    //     console2.log(`Currently In-Focus Record Id: ${inFocusRecordId}`);
-    //     console2.log(`          Selected Record Id: ${selectedRecordId}`);
+    // async function getFive9MetaData() {
+    //     try {
+    //         console.info("#### Script: >>> getFive9MetaData");
 
-    //     // update the CAVs on the call if mismatch between the current record id and the CAV
-    //     if (selectedRecordId != inFocusRecordId) {
-    //       // obtain confirmation from the agent before proceeding
-    //       console2.log(
-    //         `Selected Record Id: ${selectedRecordId} does not match ${inFocusRecordId}`
-    //       );
-    //       return Promise.resolve({
-    //         status: {
-    //           statusCode: Five9.CrmSdk.HookStatusCode.Confirmation,
-    //           message: `The record selected doesn't match the one in focus (${inFocusRecordId}).  Do you want to proceed?`,
-    //           messageHeader: "Confirmation",
-    //         },
-    //       });
+    //         var response = await fetch("https://app.five9.com/appsvcs/rs/svc/auth/metadata", {
+    //             cache: "no-cache",
+    //             credentials: "include", // include, same-origin, *omit
+    //             mode: "cors", // no-cors, cors, *same-origin.
+    //         })
+
+    //         console.info(`#### Script: getFive9MetaData returned status ${response.status}`);
+
+    //         let f9md;
+    //         if (response.status === 200) {
+    //             f9md = await response.json();
+    //         } else {
+    //             throw `getFive9MetaData returned status ${response.status}`;
+    //         }
+    //         _five9Metadata = f9md;
+
+    //     } catch (err) {
+    //         console.error("#### Script: getFive9MetaData failed: " + err);
+    //         throw err;
     //     }
-    //     console2.log(`No CAV update required`);
-    //     return Promise.resolve({
-    //       status: {
-    //         statusCode: Five9.CrmSdk.HookStatusCode.Proceed,
-    //       },
-    //     });
-    //   },
-    // });
 
-    // subscribe to WsEvents
-    interactionApi.subscribeWsEvent({
+    // }
 
-      // Event 4 is Interaction Updated
-      "4": function (payLoad, context) {
-        console2.debug("Interaction Updated")
-        console2.debug("context:", context)
-        console2.debug("context:", payLoad)
-      },
+    // async function getCallData() {
+    //     console.info("#### Script: >>> getCallData");
+    //     try {
 
-      // Event 5 is Interaction Deleted (call ended and dispositioned)
-      "5": function (payLoad, context) {
-        console2.debug("Interaction Ended and Dispositioned")
-        console2.debug("context:", context)
-        console2.debug("context:", payLoad)
-      },
-      
-      // 8 - 10 are for Preview events
-      "8": function (payLoad, context) {
-        console2.debug("Preview Created")
-        console2.debug("context:", context)
-        console2.debug("context:", payLoad)
-      },
-      
-      "9": function (payLoad, context) {
-        console2.debug("Preview Deleted / Declined")
-        console2.debug("context:", context)
-        console2.debug("context:", payLoad)
-      },
-      
-      "10": function (payLoad, context) {
-        console2.debug("Preview Updated")
-        console2.debug("context:", context)
-        console2.debug("context:", payLoad)
-      }
+    //         let response = await fetch(
+    //             "https://" +
+    //             _five9Metadata.metadata.dataCenters[0].apiUrls[0].host +
+    //             "/appsvcs/rs/svc/agents/" +
+    //             _five9Metadata.userId +
+    //             "/interactions/calls",
+    //             {
+    //                 method: "GET",
+    //                 cache: "no-cache",
+    //                 credentials: "include", // include, same-origin, *omit
+    //                 mode: "cors", // no-cors, cors, *same-origin.
+    //             })
+
+    //         let calls = await response.json();
+
+    //         console.info(`Script: Got Calls`);
+    //         console.info(calls);
+
+    //         _activeCall = null;
+
+    //         if (Array.isArray(calls) && calls.length > 0) {
+    //             _activeCall = calls[0];
+    //         }
+
+    //         if (_activeCall) {
+    //             console.info(`Script: Found active call`);
+    //             console.dir(_activeCall);
+    //         } else {
+    //             throw "Script: No active call found."
+    //         }
+
+    //     } catch (err) {
+    //         console.error("Script: getCallData failed: " + err);
+    //         throw err;
+    //     }
+    // }
+
+    // async function handleCallStarted(interactionSubscriptionEvent) {
+    //     try {
+    //         console.log("#### Call Started", interactionSubscriptionEvent);
+
+    //         await getFive9MetaData();
+    //         await getCallData();
+
+    //         console.log("#### Finished post-callStarted API flow");
+    //         console.log("#### _five9Metadata:", _five9Metadata);
+    //         console.log("#### _activeCall:", _activeCall);
+
+    //         // Put your next custom logic here
+
+    //     } catch (err) {
+    //         console.error("#### handleCallStarted failed:", err);
+    //     }
+    // }
+
+    // async function handleCallAccepted(interactionSubscriptionEvent) {
+    //     try {
+    //         console.log("#### Call Accepted", interactionSubscriptionEvent);
+
+    //         // await getFive9MetaData();
+    //         // await getCallData();
+
+    //         // console.log("#### Finished post-callStarted API flow");
+    //         // console.log("#### _five9Metadata:", _five9Metadata);
+    //         console.log("#### _activeCall:", _activeCall);
+
+    //         // Put your next custom logic here
+    //         await initialize();
+    //         const domainCavs = await getDomainCallVariables();
+    //         console.log('####', "Domain CAVs:");
+    //         console.log('####', domainCavs);
+
+    //     } catch (err) {
+    //         console.error("#### handleCallAccepted failed:", err);
+    //     }
+    // }
+
+    function loadSdkInIframe(url, callback) {
+        const iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+
+        iframe.onload = () => {
+            console.log("#### Iframe loaded, injecting script: " + url);
+            loadScriptInIframe(iframe, url)
+                .then(Five9Sdk => callback(Five9Sdk))
+                .catch(error => console.error("#### Error loading Five9 SDK:", error));
+        };
+
+        function loadScriptInIframe(iframe, url) {
+            return new Promise((resolve, reject) => {
+                console.log("#### Loading SDK Script inside iframe: " + url);
+                const script = iframe.contentDocument.createElement('script');
+                script.type = 'text/javascript';
+                script.src = url;
+                script.async = true;
+
+                script.onload = () => resolve(iframe.contentWindow.Five9);
+                script.onerror = () => reject(new Error("#### Failed to load script: " + url));
+
+                iframe.contentDocument.head.appendChild(script);
+            });
+        }
+
+        iframe.src = 'about:blank';
+    }
+
+    loadSdkInIframe('https://cdn.prod.us.five9.net/stable/crm-sdk-lib/five9.crm.sdk.js', function (Five9Sdk) {
+        console.log("#### #### SDK loaded");
+
+        if (!Five9Sdk || !Five9Sdk.CrmSdk) {
+            console.error("#### SDK load failed or CrmSdk is undefined");
+            return;
+        }
+
+        const interactionApi = Five9Sdk.CrmSdk.interactionApi();
+
+        // interactionApi.subscribe({
+        //     callStarted: async (interactionSubscriptionEvent) => {
+        //         await handleCallStarted(interactionSubscriptionEvent);
+        //     }
+        // });
+
+        interactionApi.subscribe({
+            // callAccepted: async (interactionSubscriptionEvent) => {
+            //     console.log("#### interactionApi.subscribe -> callAccepted");
+            //     console.log("#### Call Accepted", interactionSubscriptionEvent);
+            //     // // Add your custom logic here
+            //     await handleCallAccepted(interactionSubscriptionEvent);
+            // }
+            callAccepted: params => {
+                interactionApi.getCav({interactionId: params.callData.interactionId, interactionSubType: params.callData.interactionSubType})
+                .then(cavList => {
+                    console.debug('Interaction API got cavList: ' + JSON.stringify(cavList));
+                });
+            }
+        });
     });
+});
